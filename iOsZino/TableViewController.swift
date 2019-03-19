b498befdf454b934746c6053713b107a900dc211
Altamore Quentin <quentin.altamore@estiam.com>
2019-03-19 18:51:58 +0100

    Rien ne va. Je n'arrive pas à parser mes données back en Array ou JSON, et je n'arrive pas à utiliser la search bar

--- iOsZino/TableViewController.swift
+++ iOsZino/TableViewController.swift
@@ -8,9 +8,22 @@
 
 import UIKit
 
+struct Pizza: Codable {
+    var name: String
+    var ingredients: [String]
+}
+
 class TableViewController: UITableViewController {
     
-    var pizze = ["Calzone", "Regina", "Thon"]
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet var pizzeList: UITableView!
+    
+    /*var pizze = ["Calzone", "Regina", "Thon"]*/
+    var pizze = [
+        Pizza(name: "Calzone", ingredients: ["oeuf", "jambon", "mozarella", "sauce tomate"]),
+        Pizza(name: "4 Frômages", ingredients: ["gorgonzola", "mozarella", "chèvre", "parmesan", "sauce tomate"])
+    ]
+    var pizzeToShow = [Pizza]()
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -30,31 +43,50 @@ class TableViewController: UITableViewController {
         
         // create post request
         let url = URL(string: "http://192.168.43.224:8080/getRecipes")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
         
         // insert json data to the request
         //request.httpBody = jsonData
         
-        let task = session.dataTask(with: request) { data, response, error in
+        let task = session.dataTask(with: url) { data, response, error in
             guard let data = data, error == nil else {
                 print(error?.localizedDescription ?? "No data")
                 return
             }
-            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-            if let responseJSON = responseJSON as? [String: Any] {
-                print(responseJSON)
-            }
+            var postData = String(data: data, encoding: .utf8)!
+            //print(postData)
         }
         
         task.resume()
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
         
-        let pizza = pizze[indexPath.row]
+        if pizzeToShow.isEmpty {
+            pizzeToShow = pizze
+        }
+        
+        let pizza = pizzeToShow[indexPath.row].name
         cell.textLabel?.text = pizza
-        cell.detailTextLabel?.text = "Test"
+        //cell.detailTextLabel?.text = "Test"
         cell.imageView?.image = UIImage(named: pizza)
         return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        print("search Text : ", searchText)
+        
+        if searchText.isEmpty {
+            pizzeToShow = pizze
+        }
+        for pizza in pizze {
+            for ingredient in pizza.ingredients {
+                if ingredient == searchText {
+                    pizzeToShow.append(pizza)
+                }
+            }
+        }
+        
+        pizzeList.reloadData()
+        
     }
 }
